de5047e73454620718ba505b3d9fec7130322732
AlkanV <alkanv@sabanciuniv.edu>
2017-10-21 15:36:00 +0300

    Basic changes on components of MainForm

--- CS408_Client/FormMain.Designer.cs
+++ CS408_Client/FormMain.Designer.cs
@@ -42,18 +42,19 @@
             // listUsers
             // 
             this.listUsers.FormattingEnabled = true;
-            this.listUsers.ItemHeight = 25;
-            this.listUsers.Location = new System.Drawing.Point(12, 72);
+            this.listUsers.Location = new System.Drawing.Point(6, 37);
+            this.listUsers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listUsers.Name = "listUsers";
-            this.listUsers.Size = new System.Drawing.Size(290, 454);
+            this.listUsers.Size = new System.Drawing.Size(147, 238);
             this.listUsers.TabIndex = 0;
             // 
             // btnDisconnect
             // 
             this.btnDisconnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDisconnect.Location = new System.Drawing.Point(777, 560);
+            this.btnDisconnect.Location = new System.Drawing.Point(388, 291);
+            this.btnDisconnect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDisconnect.Name = "btnDisconnect";
-            this.btnDisconnect.Size = new System.Drawing.Size(183, 59);
+            this.btnDisconnect.Size = new System.Drawing.Size(92, 31);
             this.btnDisconnect.TabIndex = 1;
             this.btnDisconnect.Text = "Disconnect";
             this.btnDisconnect.UseVisualStyleBackColor = true;
@@ -61,18 +62,22 @@
             // btnGetUsers
             // 
             this.btnGetUsers.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGetUsers.Location = new System.Drawing.Point(12, 560);
+            this.btnGetUsers.Location = new System.Drawing.Point(6, 291);
+            this.btnGetUsers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnGetUsers.Name = "btnGetUsers";
-            this.btnGetUsers.Size = new System.Drawing.Size(290, 59);
+            this.btnGetUsers.Size = new System.Drawing.Size(145, 31);
             this.btnGetUsers.TabIndex = 2;
             this.btnGetUsers.Text = "Get Users";
             this.btnGetUsers.UseVisualStyleBackColor = true;
             // 
             // txtInformation
             // 
-            this.txtInformation.Location = new System.Drawing.Point(314, 72);
+            this.txtInformation.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtInformation.Location = new System.Drawing.Point(157, 37);
+            this.txtInformation.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtInformation.Name = "txtInformation";
-            this.txtInformation.Size = new System.Drawing.Size(646, 390);
+            this.txtInformation.ReadOnly = true;
+            this.txtInformation.Size = new System.Drawing.Size(325, 205);
             this.txtInformation.TabIndex = 3;
             this.txtInformation.Text = "";
             // 
@@ -80,9 +85,10 @@
             // 
             this.lblUserList.AutoSize = true;
             this.lblUserList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUserList.Location = new System.Drawing.Point(12, 22);
+            this.lblUserList.Location = new System.Drawing.Point(6, 11);
+            this.lblUserList.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblUserList.Name = "lblUserList";
-            this.lblUserList.Size = new System.Drawing.Size(130, 31);
+            this.lblUserList.Size = new System.Drawing.Size(68, 17);
             this.lblUserList.TabIndex = 4;
             this.lblUserList.Text = "User List:";
             // 
@@ -90,9 +96,10 @@
             // 
             this.lblInformation.AutoSize = true;
             this.lblInformation.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblInformation.Location = new System.Drawing.Point(349, 22);
+            this.lblInformation.Location = new System.Drawing.Point(174, 11);
+            this.lblInformation.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblInformation.Name = "lblInformation";
-            this.lblInformation.Size = new System.Drawing.Size(231, 31);
+            this.lblInformation.Size = new System.Drawing.Size(121, 17);
             this.lblInformation.TabIndex = 5;
             this.lblInformation.Text = "Lobby Information";
             // 
@@ -100,36 +107,39 @@
             // 
             this.lblMessage.AutoSize = true;
             this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMessage.Location = new System.Drawing.Point(308, 495);
+            this.lblMessage.Location = new System.Drawing.Point(154, 257);
+            this.lblMessage.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMessage.Name = "lblMessage";
-            this.lblMessage.Size = new System.Drawing.Size(132, 31);
+            this.lblMessage.Size = new System.Drawing.Size(69, 17);
             this.lblMessage.TabIndex = 6;
             this.lblMessage.Text = "Message:";
             // 
             // txtMessage
             // 
-            this.txtMessage.Location = new System.Drawing.Point(446, 487);
+            this.txtMessage.Location = new System.Drawing.Point(223, 253);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtMessage.Multiline = true;
             this.txtMessage.Name = "txtMessage";
-            this.txtMessage.Size = new System.Drawing.Size(396, 51);
+            this.txtMessage.Size = new System.Drawing.Size(200, 27);
             this.txtMessage.TabIndex = 7;
             this.txtMessage.Text = "Message...";
             // 
             // btnSend
             // 
             this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSend.Location = new System.Drawing.Point(859, 487);
+            this.btnSend.Location = new System.Drawing.Point(430, 253);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(101, 51);
+            this.btnSend.Size = new System.Drawing.Size(50, 27);
             this.btnSend.TabIndex = 9;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = true;
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(974, 635);
+            this.ClientSize = new System.Drawing.Size(487, 330);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.txtMessage);
             this.Controls.Add(this.lblMessage);
@@ -140,6 +150,7 @@
             this.Controls.Add(this.btnDisconnect);
             this.Controls.Add(this.listUsers);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormMain";
             this.Text = "Client";
             this.ResumeLayout(false);
