93148e6dc51989e89c946de4254ba49f89d7795d
AlkanV <alkanv@sabanciuniv.edu>
2017-12-14 11:49:22 +0300

    Part 3 | game gui done

--- CS408_Client/FormGame.Designer.cs
+++ CS408_Client/FormGame.Designer.cs
@@ -29,36 +29,57 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblGuessedNumber = new System.Windows.Forms.Label();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.txtGuessedNumber = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(251, 178);
+            this.button1.Location = new System.Drawing.Point(271, 139);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(296, 60);
+            this.button1.Size = new System.Drawing.Size(144, 60);
             this.button1.TabIndex = 0;
             this.button1.Text = "Surrender!";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label1
+            // lblGuessedNumber
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F);
-            this.label1.Location = new System.Drawing.Point(165, 72);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(483, 42);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "BEST GAME EVER!!1one!1";
+            this.lblGuessedNumber.AutoSize = true;
+            this.lblGuessedNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuessedNumber.Location = new System.Drawing.Point(12, 68);
+            this.lblGuessedNumber.Name = "lblGuessedNumber";
+            this.lblGuessedNumber.Size = new System.Drawing.Size(209, 29);
+            this.lblGuessedNumber.TabIndex = 1;
+            this.lblGuessedNumber.Text = "Guessed Number:";
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(12, 139);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(139, 60);
+            this.btnGuess.TabIndex = 2;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            // 
+            // txtGuessedNumber
+            // 
+            this.txtGuessedNumber.Location = new System.Drawing.Point(271, 66);
+            this.txtGuessedNumber.Name = "txtGuessedNumber";
+            this.txtGuessedNumber.Size = new System.Drawing.Size(144, 31);
+            this.txtGuessedNumber.TabIndex = 3;
             // 
             // FormGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(768, 289);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(434, 217);
+            this.Controls.Add(this.txtGuessedNumber);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.lblGuessedNumber);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "FormGame";
             this.Text = "The Game";
             this.ResumeLayout(false);
@@ -69,6 +90,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblGuessedNumber;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.TextBox txtGuessedNumber;
     }
 }
